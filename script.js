bc663fb29a411dc9a7ec6c1cf26d8baa2bba84e2
k4zam1 <56730772+k4zam1@users.noreply.github.com>
2020-04-27 13:16:15 +0900

    Add Animal

--- script.js
+++ script.js
@@ -56,14 +56,101 @@ class Point {
     }
 }
 
+function sum(arr){
+    var s = 0;
+    arr.forEach(function(elm){
+        s += elm;
+    });
+    return s;
+}
+
+function getRandomInt(min,max){
+    var rand = Math.floor(Math.random()*(max+1-min))+min;
+    return rand;
+}
+
+class Animal {
+    constructor(x,y,energy,direction,genes){
+        // num
+        this.x = x;
+        this.y = y;
+        this.energy = energy;
+        this.dir = direction;
+
+        // array
+        this.genes = genes;
+    }
+
+    move(){
+        // xについての移動
+        if(2 <= this.dir && this.dir < 5){
+            this.x += 5;
+        }
+        else if(this.dir == 1 || this.dir == 5){
+            this.x += 0;
+        }
+        else {
+            this.x -= 5;
+        }
+
+        // yについての移動
+        if(0 <= this.dir && this.dir < 3){
+            this.y -= 5;
+        }
+        else if(4 <= this.dir && this.dir < 7){
+            this.y += 5;
+        }
+
+        this.x = this.x % (canvas.width-1);
+        this.y = this.y % canvas.height;
+        this.energy -= 1;
+    }
+
+    turn(){
+        var s = sum(this.genes);
+        var r = getRandomInt(0,s);
+
+        var i = 0;
+        for(var gene of this.genes){
+            r -= gene;
+            if(r <= 0){
+                this.dir = i;
+                break;
+            }
+            i++;
+        }
+    }
+
+    //todo: eat
+
+    reproduce(){
+        if(this.energy < this.reproduction_energy){
+            return;
+        }
+
+        var energy = Math.floor(this.energy/2);
+        var genes = Object.create(this.genes);
+
+        // 突然変異 -1,0,1をランダムのスロットに加算
+        var slot = getRandomInt(0,7);
+        genes[slot] = Math.max(0,genes[slot]+getRandomInt(0,2)-1);
+
+        var child = new Animal(this.x,this.y,energy,this.dir,genes);
+        return child;
+    }
+}
+
 // draw test
 var f1 = new Point(5,5);
 var f2 = new Point(24,60);
+var adam_genes = [0,2,3,5,6,8,1,0];
+var adam = new Animal(50,50,80,0,adam_genes);
 
 var foods = [f1,f2];
 var animals = [];
 var biome = [];
 var day = 0;
+animals.push(adam);
 
 function gameRoutine(){
     // fresh screen
@@ -71,9 +158,36 @@ function gameRoutine(){
 
     // update world
     ++day;
+    
+    // animals_update
+    var new_animals = []
+    for(animal of animals){
+        if(animal.energy > 0){
+            new_animals.push(animal);
+        }
+    }
+    animals = Object.create(new_animals);
+    var childs = [];
+    for(animal of animals){
+        animal.turn();
+        animal.move();
+        //animal.eat();
+        if(day%3 == 0){
+            var child = animal.reproduce();
+            childs.push(child);
+        }
+    }
+    for(child of childs){
+        animals.push(child);
+    }
+    
     context.fillStyle = "rgb(0,200,0)";
     for(food of foods){
         context.fillRect(food.x-2,food.y-3,5,5);
+    }
+    context.fillStyle = "rgb(200,0,0)";
+    for(animal of animals){
+        context.fillRect(animal.x-2,animal.y-3,5,5);
     }
     
     // draw screen
@@ -81,3 +195,5 @@ function gameRoutine(){
 
     return day
 }
+
+
